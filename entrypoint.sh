659125d071198afd0a8a0354adf973b681883079
sahil-encardio <sahil.das@encardio.com>
2025-11-19 09:43:58 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # Ejecutar binario en background y silenciar logs
 /app/main &
-python /app/SBus_python/mqtt_pub.py --fs-grafana 10 --fs-influx 1200
+python /app/SBus_python/mqtt_pub.py --fs-grafana 10 --fs-influx 1200000000
